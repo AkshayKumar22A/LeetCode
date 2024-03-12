6c5e06c7358c89ea9f28160859ac2fc82d8aaa3a
Akshay Kumar <75368733+AkshayKumar22A@users.noreply.github.com>
2024-03-12 21:26:57 +0530

    Time: 4 ms (58.68%), Space: 71.5 MB (98.79%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,25 +1,23 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int pos[] = new int[nums.length/2];
-		int nev[] = new int[nums.length/2];
-		int idx1 = 0;
-		int idx2 = 0;
-		for(int i=0;i<nums.length;i++) {
-			if(nums[i] < 0) {
-				nev[idx1] = nums[i];
-				idx1++;
-			}
-			if(nums[i]>0) {
-				pos[idx2] = nums[i];
-				idx2++;
-			}
-		}
-		int idx = 0;
-		for(int i=0;i<nums.length/2;i++) {
-			nums[idx] = pos[i];
-			nums[idx+1] = nev[i];
-			idx+=2;
-		}
+        int pos[] = new int[nums.length / 2];
+        int nev[] = new int[nums.length / 2];
+        int idx1 = 0;
+        int idx2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                nev[idx1] = nums[i];
+                idx1++;
+            }
+            if (nums[i] > 0) {
+                pos[idx2] = nums[i];
+                idx2++;
+            }
+        }
+        for (int i = 0; i < nums.length / 2; i++) {
+            nums[2 * i] = pos[i];
+            nums[2 * i + 1] = nev[i];
+        }
         return nums;
     }
 }
