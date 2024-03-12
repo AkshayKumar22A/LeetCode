0814f92479717a5c50c24f3cc99f60c033454d4d
Akshay Kumar <75368733+AkshayKumar22A@users.noreply.github.com>
2024-03-12 23:11:04 +0530

    Time: 3 ms (55.77%), Space: 42.7 MB (96.59%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
 		Arrays.sort(nums2);
-		ArrayList<Integer> list = new ArrayList<>();
+		int[] result = new int[Math.min(nums1.length, nums2.length)];
 		
 		int i=0, j=0, idx=0;
 		
@@ -12,19 +12,18 @@ class Solution {
 			} else if (nums1[i]>nums2[j]){
 				j++;
 			} else {
-				if(list.contains(nums1[i])) {
+                result[idx++] = nums1[i];
+                i++;
+                j++;
+
+				while (i < nums1.length && nums1[i] == nums1[i - 1]) {
                     i++;
+                }
+                while (j < nums2.length && nums2[j] == nums2[j - 1]) {
                     j++;
-					continue;
-				} else {
-					list.add(nums1[i]);
-				}
-				i++;
-				j++;
+                }
 			}
 		}
-		int[] sol = list.stream().mapToInt(Integer::intValue).toArray();
-        return sol;
-
+		return Arrays.copyOf(result, idx);
     }
 }
