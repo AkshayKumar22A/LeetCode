925a020b8124623555da472e3463bce55ebe8676
Akshay Kumar <75368733+AkshayKumar22A@users.noreply.github.com>
2024-03-15 19:02:20 +0530

    Time: 2 ms (73.94%), Space: 54.6 MB (27.13%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        return product(nums);
-    }
-
-    public static int[] product(int arrf[])
-	{
-		int n=arrf.length;
-		int LP[] = new int [n];
+        int len = nums.length;
+		int LP[] = new int[nums.length];
 		LP[0] = 1;
-		for(int i=1;i<n;i++)
-		{
-			LP[i] = LP[i-1] * arrf[i-1];
+		for(int i=1;i<nums.length;i++) {
+			LP[i] = LP[i-1] * nums[i-1];
 		}
 		
-		int RP[] = new int [n];
-		RP[n-1] = 1;
-		for(int i=n-2;i>=0;i--)
-		{
-			RP[i] = RP[i+1] * arrf[i+1];
+		int RP[] = new int[nums.length];
+		RP[nums.length-1] = 1;
+		for(int i=nums.length-2;i>=0;i--) {
+			RP[i] = RP[i+1] * nums[i+1];
 		}
 		
-		for(int i=0;i<n;i++)
-		{
+		for(int i=0;i<nums.length;i++) {
 			LP[i] = LP[i] * RP[i];
 		}
 		return LP;
-		
-	}
+    }
 }
