9d6a4e1423ff1d15da71539756c8cbd631cf2713
Akshay Kumar <akshaykumar22a@gmail.com>
2025-09-30 00:55:38 +0530

    Time: 0 ms (100%), Space: 18.9 MB (27.54%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,15 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start = 0
-        end = len(nums)-1
-        while start <= end:
+        start = -1
+        end = len(nums)
+        while end - start > 1:
             mid = start + ((end - start) // 2)
             if nums[mid] == target:
                 return mid
             if nums[mid] < target:
-                start = mid + 1
+                start = mid
             if nums[mid] > target:
-                end = mid - 1
+                end = mid
         return -1
         _import_("atexit").register(lambda: open("display_runtime.txt", 'w').write('10'))   
         
