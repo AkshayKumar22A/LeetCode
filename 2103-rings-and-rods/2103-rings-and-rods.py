2efdd1830e557462005f32b00b569827874e33f6
Akshay Kumar <akshaykumar22a@gmail.com>
2025-09-30 21:11:17 +0530

    Time: 0 ms (100%), Space: 17.9 MB (10.19%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -1,19 +1,17 @@
 class Solution:
     def countPoints(self, rings: str) -> int:
         count = 0
-        red = [0] * 10
-        blue = [0] * 10
-        green = [0] * 10
+        tracker = [''] * 10
         l = len(rings)
-        for i in range(0,l,2):
-            if rings[i] == 'R':
-                red[int(rings[i+1])] +=1
-            elif rings[i] == 'G':
-                green[int(rings[i+1])] +=1
-            elif rings[i] == 'B':
-                blue[int(rings[i+1])] +=1
+        for i in range(1,l,2):
+            if rings[i-1] == "B":
+                tracker[int(rings[i])] += "B"
+            if rings[i-1] == "G":
+                tracker[int(rings[i])] += "G"
+            if rings[i-1] == "R":
+                tracker[int(rings[i])] += "R"
         for i in range(0,10):
-            if blue[i] > 0 and red[i] > 0 and green[i] > 0:
+            if "R" in tracker[i] and "G" in tracker[i] and "B" in tracker[i]:
                 count+=1
         return count
 
