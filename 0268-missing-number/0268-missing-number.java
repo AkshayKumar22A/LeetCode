d9c1e6b5f4e3748bf11c1793dde49ff9661b9f09
Akshay Kumar <akshaykumar22a@gmail.com>
2025-10-01 19:45:14 +0530

    Time: 0 ms (100%), Space: 45 MB (94.2%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int sol[] = new int[nums.length+1];
+        int count[] = new int[nums.length+1];
         for(int i=0;i<nums.length;i++) {
-            sol[nums[i]] +=1;
+            count[nums[i]] +=1;
         }
-        int flag=0;
-        for(int i=0;i<sol.length;i++) {
-            System.out.print(sol[i]+" ");
-            if(sol[i]==0) {
-                flag = i;
+        int sol=0;
+        for(int i=0;i<count.length;i++) {
+            if(count[i]==0) {
+                sol = i;
             }
         }
-        return flag;
+        return sol;
     }
 }
