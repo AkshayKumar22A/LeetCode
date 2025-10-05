8fb718547e52bb558d6fed2b97d3fb3b86c15fa2
Akshay Kumar <akshaykumar22a@gmail.com>
2025-10-05 23:23:18 +0530

    Time: 18 ms (95.81%), Space: 32.9 MB (22.19%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,8 +4,9 @@ class Solution:
         sum = 0
         ans = float("-inf")
         for i in range(l):
+            sum += nums[i]
+            if ans < sum:
+                ans = sum
             if sum < 0:
                 sum = 0
-            sum += nums[i]
-            ans = max(sum,ans)
         return ans
