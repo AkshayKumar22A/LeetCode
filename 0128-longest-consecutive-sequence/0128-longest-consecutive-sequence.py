2e61f0d4afac54a3f4154b2356d37e8a6672c87a
Akshay Kumar <akshaykumar22a@gmail.com>
2025-10-08 20:16:34 +0530

    Time: 107 ms (10.59%), Space: 37.5 MB (12.38%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,11 +1,49 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums = set(nums)
-        ans = 0
-        for num in nums:
-            if (num-1) not in nums:
-                low = 1
-                while (low+num) in nums:
-                    low +=1
-                ans = max(low,ans)
-        return ans
+        
+        maxLength = 0
+        hashMap = {}
+
+        for i in nums:
+            if i in hashMap:
+                continue
+
+            if i - 1 not in hashMap and i + 1 not in hashMap:
+                hashMap[i] = 1
+                if hashMap[i] > maxLength:
+                    maxLength = hashMap[i]
+
+            elif i - 1 in hashMap and i + 1 not in hashMap:
+                leftEndNum = i - hashMap[i - 1]
+                newLen = hashMap[leftEndNum] + 1
+
+                hashMap[i] = newLen
+                hashMap[leftEndNum] = newLen
+
+                if newLen > maxLength:
+                    maxLength = newLen
+
+            elif i - 1 not in hashMap and i + 1 in hashMap:
+                rightEndNum = i + hashMap[i + 1]
+                newLen = hashMap[rightEndNum] + 1
+
+                hashMap[i] = newLen
+                hashMap[rightEndNum] = newLen
+
+                if newLen > maxLength:
+                    maxLength = newLen
+
+            else:
+                rightEndNum = i + hashMap[i + 1]
+                leftEndNum = i - hashMap[i - 1]
+
+                totalLen = hashMap[leftEndNum] + hashMap[rightEndNum] + 1
+
+                hashMap[leftEndNum] = totalLen
+                hashMap[rightEndNum] = totalLen
+                hashMap[i] = totalLen
+
+                if totalLen > maxLength:
+                    maxLength = totalLen
+            
+        return maxLength  
