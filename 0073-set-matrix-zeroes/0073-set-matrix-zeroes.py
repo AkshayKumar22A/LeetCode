fc00907eb4ca1d7fa0a7f30bd3ce3b0bb58424fc
Akshay Kumar <akshaykumar22a@gmail.com>
2025-10-10 00:09:33 +0530

    Time: 1 ms (81.39%), Space: 18.5 MB (54.29%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,26 +1,24 @@
 class Solution:
-    def markInfinity(self, matrix, row, col):
-        r = len(matrix)
-        c = len(matrix[0])
-        for i in range(r):
-            if matrix[i][col] != 0:  # Avoid overwriting original zeros
-                matrix[i][col] = float("inf")
-        for j in range(c):
-            if matrix[row][j] != 0:  # Avoid overwriting original zeros
-                matrix[row][j] = float("inf")
-
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        r = len(matrix)
-        c = len(matrix[0])
-        for i in range(r):
-            for j in range(c):
-                if matrix[i][j] == 0:
-                    self.markInfinity(matrix, i, j)
+        row = len(matrix)
+        col = len(matrix[0])
+        rowtrack = [0 for _ in range(row)]
+        coltrack = [0 for _ in range(col)]
 
-        for i in range(r):
-            for j in range(c):
-                if matrix[i][j] == float("inf"):
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == 0:
+                    rowtrack[i] = -1
+                    coltrack[j] = -1
+        
+        for i in range(row):
+            if rowtrack[i] == -1:
+                for j in range(col):
                     matrix[i][j] = 0
+        for i in range(col):
+            if coltrack[i] == -1:
+                for j in range(row):
+                    matrix[j][i] = 0
