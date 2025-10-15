3ad6fd2dff3065d6aa26063d19c898023ddf285f
Akshay Kumar <akshaykumar22a@gmail.com>
2025-10-15 21:22:22 +0530

    Time: 0 ms (100%), Space: 18.8 MB (93.64%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,29 +1,24 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if len(nums)-1 == 0 and nums[0] == target:
-            return [0,0]
-        if len(nums) == 0:
-            return [-1,-1]
-        lb = self.lbs(nums,target)
-        if lb > len(nums) - 1 or nums[lb] != target:
-            return [-1,-1]
-        ub = lb
-        while nums[ub] == target and ub <len(nums)-1:
-            if nums[ub+1] == target:
-                ub+=1
-            else:
-                break
-        return [lb,ub]
-    
-    def lbs(self, nums: List[int], target: int) -> List[int]:
-        start = 0
-        end = len(nums) - 1
+        left = self.binSearch(nums, target, True)
+        right = self.binSearch(nums, target, False)
+        return [left, right]
+
+    # leftBias=[True/False], if false, res is rightBiased
+    def binSearch(self, nums, target, leftBias):
+        low = 0
+        high = len(nums) - 1
         result = -1
-        while start<=end:
-            mid = start + (end-start) // 2
-            if nums[mid] >= target:
-                result = mid
-                end = mid - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if target > nums[mid]:
+                low = mid + 1
+            elif target < nums[mid]:
+                high = mid - 1
             else:
-                start = mid + 1
+                result = mid
+                if leftBias:
+                    high = mid - 1
+                else:
+                    low = mid + 1
         return result
