b7e6cf40d9a777d974f6ff6445beafa9d74ddd0c
Akshay Kumar <akshaykumar22a@gmail.com>
2025-10-15 21:22:30 +0530

    Time: 0 ms (100%), Space: 19.2 MB (20.31%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,40 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self.binSearch(nums, target, True)
-        right = self.binSearch(nums, target, False)
-        return [left, right]
+        left_occurence = self.find_left_occurence(nums, target)
 
-    # leftBias=[True/False], if false, res is rightBiased
-    def binSearch(self, nums, target, leftBias):
-        low = 0
-        high = len(nums) - 1
-        result = -1
-        while low <= high:
-            mid = (low + high) // 2
-            if target > nums[mid]:
-                low = mid + 1
-            elif target < nums[mid]:
-                high = mid - 1
+        if left_occurence == -1:
+            return [-1, -1]
+
+        right_occurence = self.find_right_occurence(nums, target)
+        return [left_occurence, right_occurence]
+    
+    def find_left_occurence(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        left_occurence = -1
+        
+        while start <= end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                left_occurence, end = mid, mid - 1
+            elif nums[mid] < target:
+                start = mid + 1
             else:
-                result = mid
-                if leftBias:
-                    high = mid - 1
-                else:
-                    low = mid + 1
-        return result
+                end = mid - 1
+        
+        return left_occurence
+
+    def find_right_occurence(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        right_occurence = -1
+        
+        while start <= end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                right_occurence, start = mid, mid + 1
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return right_occurence
