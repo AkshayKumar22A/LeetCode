4d7290ad729ec7cb842d5bdcfbcbe6678375ccc5
Akshay Kumar <akshaykumar22a@gmail.com>
2025-10-19 02:17:35 +0530

    Time: 43 ms (5.89%), Space: 19.9 MB (5.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        element = majority = 0
-        freq_map = {}
-        for i in nums:
-            freq_map[i] = freq_map.get(i,0) + 1
-            if freq_map[i] > majority:
-                element = i
-                majority = freq_map[i]
-        return element
+        count = 0
+        candidate = 0
+        for num in nums:
+            print(count)
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count+=1
+            else:
+                count-=1
+        return candidate
