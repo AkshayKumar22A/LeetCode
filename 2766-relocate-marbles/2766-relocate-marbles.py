3d38305338feb7c797fedadc59088f398f859b73
Akshay Kumar <akshaykumar22a@gmail.com>
2025-10-28 02:18:42 +0530

    Time: 51 ms (63.2%), Space: 36.9 MB (83.2%) - LeetHub

--- 2766-relocate-marbles/2766-relocate-marbles.py
+++ 2766-relocate-marbles/2766-relocate-marbles.py
@@ -1,12 +1,7 @@
 class Solution:
     def relocateMarbles(self, nums: List[int], moveFrom: List[int], moveTo: List[int]) -> List[int]:
-        i = 0
         nums = set(nums)
-        while(i < len(moveFrom)):
-            if moveFrom[i] in nums:
-                nums.remove(moveFrom[i])
-                nums.add(moveTo[i])
-            i+=1
-        return sorted(nums)
-
-        
+        for from_pos, to_pos in zip(moveFrom, moveTo):
+            nums.discard(from_pos)  # Remove the marble from current position
+            nums.add(to_pos)         # Add the marble to new position
+        return sorted(nums)        
