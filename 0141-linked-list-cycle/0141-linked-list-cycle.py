8df81107d9fd31e96a5a6ca98283b55a2f5a5a20
Akshay Kumar <akshaykumar22a@gmail.com>
2026-01-18 23:37:45 +0530

    Time: 63 ms (5.01%), Space: 22.7 MB (7.14%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -10,8 +10,7 @@ class Solution:
         fast = head
         while fast is not None and fast.next is not None:
             slow = slow.next
-            fast = fast.next
-            fast = fast.next
+            fast = fast.next.next
             if slow == fast:
                 return True
         
