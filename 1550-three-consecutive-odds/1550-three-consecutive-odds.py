16dfeae160aa930e37835257e81e46d4e54bbcd9
Akshay Kumar <akshaykumar22a@gmail.com>
2026-03-12 23:32:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.py
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -2,14 +2,12 @@ class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
         if len(arr) < 3:
             return False
-        stack = []
+        count = 0
         for i in range(len(arr)):
             if (arr[i]%2) != 0:
-                stack.append(arr[i])
+                count += 1
             else:
-                while stack:
-                    stack.pop()
-            if len(stack) == 3:
+                count = 0
+            if count == 3:
                 return True
         return False
-
