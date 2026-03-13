6b5b032ba78c94dff4512ff9f2a0340036cd83d4
Akshay Kumar <akshaykumar22a@gmail.com>
2026-03-14 00:27:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -3,10 +3,7 @@ class Solution:
         result = []
         score = 0
         for i in range(len(operations)):
-            print(result)
-            if not(operations[i] == "+" or operations[i] == "D" or operations[i] == "C"):
-                result.append(int(operations[i]))
-            elif operations[i] == "+":
+            if operations[i] == "+":
                 score1 = result[-1]
                 score2 = result[-2]
                 result.append((score1+score2))
@@ -14,4 +11,6 @@ class Solution:
                 result.append((2*result[-1]))
             elif operations[i] == "C":
                 result.pop()
+            else:
+                result.append(int(operations[i]))
         return sum(result)
