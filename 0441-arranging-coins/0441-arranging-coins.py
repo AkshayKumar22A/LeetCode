00c515623328c05a4a7eeb9460e256e7f7600589
Akshay Kumar <akshaykumar22a@gmail.com>
2026-04-19 02:11:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,11 +1,13 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        count = 0
-        i = 1
-        while n != 0:
-            if n < i:
-                break
-            count += 1
-            n = n - i
-            i+= 1
-        return count
+        ans = 0
+        start = 0
+        end = n
+        while start <= end:
+            mid = (start + end) // 2
+            if (((mid + 1) * mid) // 2) <= n:
+                ans = mid
+                start = mid + 1
+            else:
+                end = mid - 1
+        return ans
