f5f40c7f81d9df7f55ce2fb3e611d14de311242e
Akshay Kumar <akshaykumar22a@gmail.com>
2026-04-24 01:16:57 +0530

    Time: 390 ms (35.71%), Space: 19.4 MB (22.26%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,40 +1,28 @@
 class Solution:
-    def is_palindrome(self,subs):
-        return subs == subs[::-1]
-
-    def good(self,x,s):
-        for i in range(len(s) - x + 1):
-            if self.is_palindrome(s[i:i+x]):
-                return i
-        return -1    
-
-    
     def longestPalindrome(self, s: str) -> str:
         best_len = 0
         best_s = ""
 
-        for parity in [0, 1]:
-            low, high = 1, len(s)
-
-            if low % 2 != parity:
-                low += 1
-            if high % 2 != parity:
-                high -= 1
-        
-            while low <= high:
-                mid = (low + high) // 2
-                if mid % 2 != parity:
-                    mid += 1
-                if mid > high:
+        for mid in range(len(s)):
+            x = 0
+            while (mid - x) >= 0 and (mid + x) < len(s):
+                if s[mid-x] != s[mid+x]:
                     break
+                l = 2 * x + 1
+                if l > best_len:
+                    best_len = l
+                    best_s = s[mid-x:mid+x+1]
+                x+=1 
 
-                temp = self.good(mid,s)
-                if temp != -1:
-                    if mid > best_len:
-                        best_len = mid
-                        best_s = s[temp:temp+mid]
-                    low = mid + 2
-                else:
-                    high = mid - 2
+        for mid in range(len(s)-1):
+            x = 1
+            while (mid - x + 1) >= 0 and (mid + x) < len(s):
+                if s[mid-x+1] != s[mid+x]:
+                    break
+                l = 2 * x
+                if l > best_len:
+                    best_len = l
+                    best_s = s[mid-x+1:mid+x+1]
+                x += 1
 
         return best_s
